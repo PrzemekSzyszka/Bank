aa02ceba26be652607378ab123d6b88a486a1451
Przemysław Szyszka <przemeklo@o2.pl>
2014-09-19 12:38:52 +0200

    [#1] Create Money class

---
+++ Money.rb
@@ -0,0 +1,16 @@
+class Money
+  attr_reader :amount, :currency
+
+  def initialize (amount, currency)
+    @amount = amount
+    @currency = currency
+  end
+
+  def to_s
+    "#{format("%.2f", @amount)} #{@currency}"
+  end
+
+  def inspect
+    "#<Money #{to_s}>"
+  end
+end
